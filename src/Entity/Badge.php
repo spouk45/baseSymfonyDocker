9f37f28f968abbe82fc7ffbfb7be007cf451b165
Yoan <yoan.beauchamp@sigrenea.com>
2024-07-26 12:03:19 +0200

    orm en place et fonctionnement ok

---
+++ src/Entity/Badge.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BadgeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BadgeRepository::class)]
+class Badge
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?bool $authorized = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isAuthorized(): ?bool
+    {
+        return $this->authorized;
+    }
+
+    public function setAuthorized(bool $authorized): static
+    {
+        $this->authorized = $authorized;
+
+        return $this;
+    }
+}
