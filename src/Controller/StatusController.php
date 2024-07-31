1d59c972abfcf208d92951c1884917eaf5859f60
Yoan <yoan.beauchamp@sigrenea.com>
2024-07-31 18:32:43 +0200

    amélioration du firewall

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -2,14 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StatusController extends AbstractController
 {
+    #[Route('/ping', name: 'ping', methods: ['GET'])]
+    public function ping(): Response
+    {
+        return new Response('pong', Response::HTTP_OK);
+    }
+
+
     #[Route('/status', name: 'status', methods: ['GET'])]
-    public function status(): Response
+    public function status(Security $security): Response
     {
         return new Response('Server is up and running', Response::HTTP_OK);
     }
