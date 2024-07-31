b30e3247de8a2c4453071901e4e48242124a16d8
Yoan <yoan.beauchamp@sigrenea.com>
2024-07-31 11:43:09 +0200

    amélioration route

--- src/Controller/ApiBadgeController.php
+++ src/Controller/ApiBadgeController.php
@@ -11,31 +11,34 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/api', name: 'api_')]
 class ApiBadgeController extends AbstractController
 {
     private $security;
+    private $badgeRepository;
+
 
     public function __construct(Security $security, BadgeRepository $badgeRepository)
     {
         $this->security = $security;
+        $this->badgeRepository = $badgeRepository;
+
     }
 
-    #[Route('/api/badges', name: 'getBadges', methods: ['GET'])]
-    public function getBadges(BadgeRepository $badgeRepository): JsonResponse
+    #[Route('/badges', name: 'getBadges', methods: ['GET'])]
+    public function getBadges(): JsonResponse
     {
         // Obtenir l'EPCI à partir du token
         $epci = $this->security->getUser();
 
         // Récupérer les badges associés à cet EPCI
-        $badges = $badgeRepository->findBy(['epci' => $epci]);
+        $badges = $this->badgeRepository->findBy(['epci' => $epci]);
         return $this->json($badges, 200, [], ['groups' => 'main']);
     }
 
-    #[Route('/api/badges', name: 'createBadge', methods: ['POST'])]
+    #[Route('/badges', name: 'createBadge', methods: ['POST'])]
     public function createBadge(Request $request, EntityManagerInterface $em): JsonResponse
     {
-        $epci = $this->security->getUser();
-
         $data = json_decode($request->getContent(), true);
         $name = $data['name'] ?? null;
         $authorized = $data['authorized'] ?? null;
@@ -44,6 +47,7 @@ class ApiBadgeController extends AbstractController
             return $this->json(['error' => 'Invalid input'], JsonResponse::HTTP_BAD_REQUEST);
         }
 
+        $epci = $this->security->getUser();
         $badge = new Badge();
         $badge->setName($name);
         $badge->setAuthorized((bool) $authorized);
